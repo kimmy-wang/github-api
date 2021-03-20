3a0d6f1eb236e76ea4175350b5cd1fbca37b8c78
realkimmy <upcwangying@126.com>
2021-03-20 16:58:42 +0800

    feat: github contributions

--- api/contributions.js
+++ api/contributions.js
@@ -1,5 +1,8 @@
+import { fetchData } from '../utils/fetch'
+
 export default async (req, res) => {
   const { username, from, to } = req.query
+  const contributions = await fetchData(username, from, to)
   res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate')
-  res.json(username)
+  res.json(contributions)
 }
--- utils/fetch.js
+++ utils/fetch.js
@@ -1,6 +1,5 @@
 import cheerio from 'cheerio'
 import fetch from 'node-fetch'
-import _ from 'lodash'
 
 const COLOR_MAP = {
   0: '#ebedf0',
@@ -10,85 +9,23 @@ const COLOR_MAP = {
   4: '#216e39',
 }
 
-async function fetchDataForYear(url, year, format) {
-  const data = await fetch(`https://github.com${url}`)
+export async function fetchData(username, from, to) {
+  let url = `https://github.com/${username}`
+  if (from && to) {
+    url += `?from=${from}&to=${to}`
+  }
+  const data = await fetch(url)
   const $ = cheerio.load(await data.text())
   const $days = $('svg.js-calendar-graph-svg rect.ContributionCalendar-day')
-  const contribText = $('.js-yearly-contributions h2')
-    .text()
-    .trim()
-    .match(/^([0-9,]+)\s/)
-  let contribCount
-  if (contribText) {
-    ;[contribCount] = contribText
-    contribCount = parseInt(contribCount.replace(/,/g, ''), 10)
+
+  const parseDay = day => {
+    const $day = $(day)
+    return {
+      date: $day.attr('data-date'),
+      count: parseInt($day.attr('data-count') || 0, 10),
+      color: COLOR_MAP[$day.attr('data-level')],
+    }
   }
 
-  return {
-    year,
-    total: contribCount || 0,
-    range: {
-      start: $($days.get(0)).attr('data-date'),
-      end: $($days.get($days.length - 1)).attr('data-date'),
-    },
-    contributions: (() => {
-      const parseDay = day => {
-        const $day = $(day)
-        const date = $day
-          .attr('data-date')
-          .split('-')
-          .map(d => parseInt(d, 10))
-        const color = COLOR_MAP[$day.attr('data-level')]
-        const value = {
-          date: $day.attr('data-date'),
-          count: parseInt($day.attr('data-count'), 10),
-          color,
-          intensity: $day.attr('data-level') || 0,
-        }
-        return { date, value }
-      }
-
-      if (format !== 'nested') {
-        return $days.get().map(day => parseDay(day).value)
-      }
-
-      return $days.get().reduce((o, day) => {
-        const { date, value } = parseDay(day)
-        const [y, m, d] = date
-        if (!o[y]) o[y] = {}
-        if (!o[y][m]) o[y][m] = {}
-        o[y][m][d] = value
-        return o
-      }, {})
-    })(),
-  }
-}
-
-export async function fetchDataForAllYears(username, format) {
-  const years = await fetchYears(username)
-  return Promise.all(years.map(year => fetchDataForYear(year.href, year.text, format))).then(
-    resp => {
-      return {
-        years: (() => {
-          const obj = {}
-          const arr = resp.map(year => {
-            const { contributions, ...rest } = year
-            _.setWith(obj, [rest.year], rest, Object)
-            return rest
-          })
-          return format === 'nested' ? obj : arr
-        })(),
-        contributions:
-          format === 'nested'
-            ? resp.reduce((acc, curr) => _.merge(acc, curr.contributions))
-            : resp
-                .reduce((list, curr) => [...list, ...curr.contributions], [])
-                .sort((a, b) => {
-                  if (a.date < b.date) return 1
-                  else if (a.date > b.date) return -1
-                  return 0
-                }),
-      }
-    },
-  )
+  return $days.get().map(parseDay)
 }
